9643783a5a5cd7d3a1bab5afe88b7464b30bb936
luosonglin <iluosonglin@qq.com>
2018-06-19 09:50:24 +0800

    蓝牙连接

---
+++ app/src/main/java/com/kaifabang/bluetoothapplication/DeviceControlActivity.java
@@ -0,0 +1,278 @@
+package com.kaifabang.bluetoothapplication;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattService;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ExpandableListView;
+import android.widget.SimpleExpandableListAdapter;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by luosonglin on 6/17/18.
+ */
+
+@SuppressLint("NewApi")
+public class DeviceControlActivity extends Activity {
+    private final static String TAG = DeviceControlActivity.class
+            .getSimpleName();
+
+    public static final String EXTRAS_DEVICE_NAME = "DEVICE_NAME";
+    public static final String EXTRAS_DEVICE_ADDRESS = "DEVICE_ADDRESS";
+
+    private TextView mConnectionState;
+    private TextView mDataField;
+    private String mDeviceName;
+    private String mDeviceAddress;
+    private ExpandableListView mGattServicesList;
+    public static BluetoothGatt mBluetoothGatt;
+    private BluetoothLeService mBluetoothLeService;
+    private ArrayList<ArrayList<BluetoothGattCharacteristic>> mGattCharacteristics = new ArrayList<ArrayList<BluetoothGattCharacteristic>>();
+
+    private boolean mConnected = false;
+
+    private final String LIST_NAME = "NAME";
+    private final String LIST_UUID = "UUID";
+
+    // Code to manage Service lifecycle.
+    private final ServiceConnection mServiceConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName componentName,
+                                       IBinder service) {
+            mBluetoothLeService = ((BluetoothLeService.LocalBinder) service).getService();
+            if (!mBluetoothLeService.initialize()) {
+                Log.e(TAG, "Unable to initialize Bluetooth");
+                finish();
+            }
+            // Automatically connects to the device upon successful start-up
+            // initialization.
+            mBluetoothLeService.connect(mDeviceAddress);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            mBluetoothLeService = null;
+        }
+    };
+
+    // Handles various events fired by the Service.
+    // ACTION_GATT_CONNECTED: connected to a GATT server.
+    // ACTION_GATT_DISCONNECTED: disconnected from a GATT server.
+    // ACTION_GATT_SERVICES_DISCOVERED: discovered GATT services.
+    // ACTION_DATA_AVAILABLE: received data from the device. This can be a result of read or notification operations.
+    private final BroadcastReceiver mGattUpdateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+            Log.e(TAG, "ddddd mGattUpdateReceiver "+ action);
+            if (BluetoothLeService.ACTION_GATT_CONNECTED.equals(action)) {
+                mConnected = true;
+                isConnected();
+                updateConnectionState(R.string.connected);
+            } else if (BluetoothLeService.ACTION_GATT_DISCONNECTED.equals(action)) {
+                mConnected = false;
+                isConnected();
+                updateConnectionState(R.string.disconnected);
+                clearUI();
+            } else if (BluetoothLeService.ACTION_GATT_SERVICES_DISCOVERED.equals(action)) {
+                displayGattServices(mBluetoothLeService.getSupportedGattServices());
+            } else if (BluetoothLeService.ACTION_DATA_AVAILABLE.equals(action)) {
+                displayData(intent.getStringExtra(BluetoothLeService.EXTRA_DATA));
+            }
+        }
+    };
+
+    private void clearUI() {
+        mGattServicesList.setAdapter((SimpleExpandableListAdapter) null);
+        mDataField.setText(R.string.no_data);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gatt_services_characteristics);
+
+//        java.lang.SecurityException: Need ACCESS_COARSE_LOCATION or ACCESS_FINE_LOCATION permission to get scan results
+        mDeviceName = getIntent().getStringExtra(EXTRAS_DEVICE_NAME);
+        mDeviceAddress = getIntent().getStringExtra(EXTRAS_DEVICE_ADDRESS);
+
+        // Sets up UI references.
+        ((TextView) findViewById(R.id.device_address)).setText(mDeviceAddress);
+        mConnectionState = (TextView) findViewById(R.id.connection_state);
+        mDataField = (TextView) findViewById(R.id.data_value);
+
+        ((TextView) findViewById(R.id.name)).setText(mDeviceName);
+
+        Intent gattServiceIntent = new Intent(this, BluetoothLeService.class);
+        boolean bll = bindService(gattServiceIntent, mServiceConnection,
+                BIND_AUTO_CREATE);
+        if (bll) {
+            System.out.println("---------------");
+        } else {
+            System.out.println("===============");
+        }
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //...
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(mGattUpdateReceiver, makeGattUpdateIntentFilter());
+        if (mBluetoothLeService != null) {
+            final boolean result = mBluetoothLeService.connect(mDeviceAddress);
+            Log.d(TAG, "Connect request result=" + result);
+        }
+
+        isConnected();
+    }
+
+    private void isConnected(){
+        if (mConnected) {
+            ((Button)findViewById(R.id.button_connect_service)).setVisibility(View.GONE);
+            ((Button)findViewById(R.id.button_disconnect_service)).setVisibility(View.VISIBLE);
+            ((Button)findViewById(R.id.button_disconnect_service)).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mBluetoothLeService.disconnect();
+                }
+            });
+        } else {
+            ((Button)findViewById(R.id.button_connect_service)).setVisibility(View.VISIBLE);
+            ((Button)findViewById(R.id.button_connect_service)).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mBluetoothLeService.connect(mDeviceAddress);
+                }
+            });
+            ((Button)findViewById(R.id.button_disconnect_service)).setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(mGattUpdateReceiver);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbindService(mServiceConnection);
+        mBluetoothLeService = null;
+    }
+
+    private void updateConnectionState(final int resourceId) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mConnectionState.setText(resourceId);
+            }
+        });
+    }
+    StringBuffer sb = new StringBuffer();
+    private void displayData(String data) {
+        sb.append(data);
+        Log.d(TAG, "displayData: "+sb.toString());
+    }
+
+    // Demonstrates how to iterate through the supported GATT
+    // Services/Characteristics.
+    // In this sample, we populate the data structure that is bound to the
+    // ExpandableListView
+    // on the UI.
+    private void displayGattServices(List<BluetoothGattService> gattServices) {
+        if (gattServices == null)
+            return;
+        String uuid = null;
+
+        Log.e(TAG, "displayGattServices() "+gattServices.size());
+        // Loops through available GATT Services.
+        for (BluetoothGattService i : gattServices) {
+            uuid = i.getUuid().toString();
+            Log.d(TAG, "displayGattServices: "+uuid);
+            List<BluetoothGattCharacteristic> gattCharacteristics = i.getCharacteristics();
+            for (BluetoothGattCharacteristic gattCharacteristic : gattCharacteristics) {
+                uuid = gattCharacteristic.getUuid().toString();
+                if (uuid.contains("fff4")) {
+                    Log.e("console", "2gatt Characteristic: " + uuid);
+                    mBluetoothLeService.setCharacteristicNotification(gattCharacteristic, true);
+                    mBluetoothLeService.readCharacteristic(gattCharacteristic);
+                }
+            }
+        }
+    }
+
+    /**
+     *
+     * @return
+     */
+    private static IntentFilter makeGattUpdateIntentFilter() {
+        final IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(BluetoothLeService.ACTION_GATT_CONNECTED);
+        intentFilter.addAction(BluetoothLeService.ACTION_GATT_DISCONNECTED);
+        intentFilter.addAction(BluetoothLeService.ACTION_GATT_SERVICES_DISCOVERED);
+        intentFilter.addAction(BluetoothLeService.ACTION_DATA_AVAILABLE);
+        intentFilter.addAction(BluetoothLeService.EXTRA_DATA);
+        return intentFilter;
+    }
+
+    /**
+     * 将16进制 转换成10进制
+     *
+     * @param str
+     * @return
+     */
+    public static String print10(String str) {
+
+        StringBuffer buff = new StringBuffer();
+        String array[] = str.split(" ");
+        for (int i = 0; i < array.length; i++) {
+            int num = Integer.parseInt(array[i], 16);
+            buff.append(String.valueOf((char) num));
+        }
+        return buff.toString();
+    }
+
+    /**
+     * byte转16进制
+     *
+     * @param b
+     * @return
+     */
+    public static String byte2HexStr(byte[] b) {
+
+        String stmp = "";
+        StringBuilder sb = new StringBuilder("");
+        for (int n = 0; n < b.length; n++) {
+            stmp = Integer.toHexString(b[n] & 0xFF);
+            sb.append((stmp.length() == 1) ? "0" + stmp : stmp);
+            sb.append(" ");
+        }
+        return sb.toString().toUpperCase().trim();
+    }
+
+}
--- app/src/main/java/com/kaifabang/bluetoothapplication/MainActivity.java
+++ app/src/main/java/com/kaifabang/bluetoothapplication/MainActivity.java
@@ -1,7 +1,11 @@
 package com.kaifabang.bluetoothapplication;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.ListActivity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
 import android.bluetooth.le.ScanCallback;
 import android.bluetooth.le.ScanResult;
 import android.content.BroadcastReceiver;
@@ -10,19 +14,30 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Handler;
 import android.os.IBinder;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.logging.Logger;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ListActivity {
 
     final String TAG = MainActivity.class.getSimpleName();
 
@@ -30,17 +45,44 @@ public class MainActivity extends AppCompatActivity {
     //系统会将其作为 requestCode 参数传递回您的 onActivityResult() 实现。
     static final int REQUEST_ENABLE_BT = 1;
 
+    // stop scan after 10 second
+    private static final long SCAN_PERIOD = 10000;
+
+    private LeDeviceListAdapter mLeDeviceListAdapter;
     private BluetoothAdapter mBluetoothAdapter;
+    private Handler mHandler;
+    private boolean mScanning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                String[] permissions = new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION};
+                ActivityCompat.requestPermissions(this, permissions, 10);
+                return;
+            }
+        }
+
+        mHandler = new Handler();
+
         //获取 BluetoothAdapter
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        // 旧版本获取BluetoothAdapter方式
+        // 初始化 Bluetooth adapter, 通过蓝牙管理器得到一个参考蓝牙适配器(API必须在以上android4.3或以上和版本)
+//        final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+//        mBluetoothAdapter = bluetoothManager.getAdapter();
+
+        // 检查设备上是否支持蓝牙
         if (mBluetoothAdapter == null) {
             // Device does not support Bluetooth
+            Toast.makeText(this, R.string.error_bluetooth_not_supported, Toast.LENGTH_SHORT).show();
+            finish();
+            return;
         }
 
         //启用蓝牙
@@ -49,17 +91,53 @@ public class MainActivity extends AppCompatActivity {
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
         }
 
-        Log.i(TAG, mBluetoothAdapter.getName()
-                + " " + mBluetoothAdapter.getAddress());
+        Log.i(TAG, mBluetoothAdapter.getName() + " " + mBluetoothAdapter.getAddress());
+    }
 
-        //获取已配对蓝牙设备
-        Set<BluetoothDevice> devices = mBluetoothAdapter.getBondedDevices();
-        Log.d(TAG, "bonded device size =" + devices.size());
-        for (BluetoothDevice bonddevice : devices) {
-            Log.d(TAG, "bonded device name =" + bonddevice.getName() + " address" + bonddevice.getAddress());
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // 为了确保设备上蓝牙能使用, 如果当前蓝牙设备没启用,弹出对话框向用户要求授予权限来启用
+        if (!mBluetoothAdapter.isEnabled()) {
+            if (!mBluetoothAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
         }
 
+        // Initializes list view adapter.
+        mLeDeviceListAdapter = new LeDeviceListAdapter();
+        setListAdapter(mLeDeviceListAdapter);
+//        scanDevice(true);
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        scanDevice(false);
+        mLeDeviceListAdapter.clear();
+    }
+
+    private void scanDevice(final boolean enable) {
+        if (enable) {
+            // Stops scanning after a pre-defined scan period.
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mScanning = false;
+                    //扫描BLE设备是电源密集型操作，浪费电量，给扫描一个时间限制
+                    mBluetoothAdapter.cancelDiscovery();
+                }
+            }, SCAN_PERIOD);
+
+            mScanning = true;
+            //搜索设备
+            mBluetoothAdapter.startDiscovery();
+        } else {
+            mScanning = false;
+            mBluetoothAdapter.cancelDiscovery();
+        }
     }
 
     @Override
@@ -68,13 +146,16 @@ public class MainActivity extends AppCompatActivity {
 
         Log.i(TAG, requestCode + " " + resultCode); //成功启动，返回1 -1
 
+        // User chose not to enable Bluetooth.
+        if (requestCode == REQUEST_ENABLE_BT && resultCode == Activity.RESULT_CANCELED) {
+            finish();
+            return;
+        }
     }
-
 
     public void search(View view) {
         //搜索设备
         mBluetoothAdapter.startDiscovery();
-
 
         IntentFilter filter = new IntentFilter();
         //发现设备
@@ -84,9 +165,9 @@ public class MainActivity extends AppCompatActivity {
         //蓝牙设备状态改变
         filter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED);
         registerReceiver(mBluetoothReceiver, filter);
-
     }
 
+    //搜索蓝牙设备，该过程是异步的，通过下面注册广播接受者，可以监听是否搜到设备。
     private BroadcastReceiver mBluetoothReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
@@ -98,24 +179,123 @@ public class MainActivity extends AppCompatActivity {
                 if (device == null || device.getName() == null) return;
                 Log.d(TAG, "name=" + device.getName() + "address=" + device.getAddress());
                 //蓝牙设备名称
-//                String name = scanDevice.getName();
-//                if (name != null && name.equals(BLUETOOTH_NAME)) {
-//                    //停止搜索
-//                    mBluetoothAdapter.cancelDiscovery();
+                String name = device.getName();
+                if (name != null && !TextUtils.isEmpty(name)) {
+                    //停止搜索
+                    mBluetoothAdapter.cancelDiscovery();
 //                    //取消扫描
 //                    mProgressDialog.setTitle(getResources().getString(R.string.progress_connecting));                    //连接到设备。
 //                    mBlthChatUtil.connect(scanDevice);
-//                }
+                    mLeDeviceListAdapter.addDevice(device);
+                    mLeDeviceListAdapter.notifyDataSetChanged();
+                }
                 Log.i(TAG, device.getName() + " " + device.getAddress() + " " + device.getBondState() + " " + device.getType() + " " + device.getUuids());
 
-
-//                devices.add(device);
-//                deviceName.add(device.getName());
-//                arrayAdapter.notifyDataSetChanged();
+                //获取已配对蓝牙设备
+//                Set<BluetoothDevice> devices = mBluetoothAdapter.getBondedDevices();
+//                Log.d(TAG, "bonded device size =" + devices.size());
+//                for (BluetoothDevice bonddevice : devices) {
+//                    Log.d(TAG, "bonded device name =" + bonddevice.getName() + " address" + bonddevice.getAddress());
+//                    mLeDeviceListAdapter.addDevice(bonddevice);
+//                    mLeDeviceListAdapter.notifyDataSetChanged();
+//                }
 
             } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
                 Toast.makeText(MainActivity.this, "扫描完毕", Toast.LENGTH_SHORT).show();
             }
         }
     };
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        final BluetoothDevice device = mLeDeviceListAdapter.getDevice(position);
+        if (device == null) return;
+        Log.i(TAG+" onListItemClick", device.getName()+" "+device.getAddress());
+
+        final Intent intent = new Intent(this, DeviceControlActivity.class);
+        intent.putExtra(DeviceControlActivity.EXTRAS_DEVICE_NAME, device.getName());
+        intent.putExtra(DeviceControlActivity.EXTRAS_DEVICE_ADDRESS, device.getAddress());
+        if (mScanning) {
+            //扫描到需要的设备后，马上停止扫描
+            mBluetoothAdapter.cancelDiscovery();
+            mScanning = false;
+        }
+        startActivity(intent);
+    }
+
+    // Adapter for holding devices found through scanning.
+    private class LeDeviceListAdapter extends BaseAdapter {
+        private ArrayList<BluetoothDevice> mLeDevices;
+        private LayoutInflater mInflator;
+
+        public LeDeviceListAdapter() {
+            super();
+            mLeDevices = new ArrayList<BluetoothDevice>();
+            mInflator = MainActivity.this.getLayoutInflater();
+        }
+
+        public void addDevice(BluetoothDevice device) {
+            if(!mLeDevices.contains(device)) {
+                mLeDevices.add(device);
+            }
+        }
+
+        public BluetoothDevice getDevice(int position) {
+            return mLeDevices.get(position);
+        }
+
+        public void clear() {
+            mLeDevices.clear();
+        }
+
+        @Override
+        public int getCount() {
+            return mLeDevices.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return mLeDevices.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            ViewHolder viewHolder;
+            // General ListView optimization code.
+            if (view == null) {
+                view = mInflator.inflate(R.layout.listitem_device, null);
+                viewHolder = new ViewHolder();
+                viewHolder.deviceAddress = (TextView) view.findViewById(R.id.device_address);
+                viewHolder.deviceName = (TextView) view.findViewById(R.id.device_name);
+                viewHolder.deviceRSSI = (TextView) view.findViewById(R.id.device_RSSI);
+                view.setTag(viewHolder);
+            } else {
+                viewHolder = (ViewHolder) view.getTag();
+            }
+
+            BluetoothDevice device = mLeDevices.get(i);
+            final String deviceName = device.getName();
+            final String deviceRSSI = BluetoothDevice.EXTRA_RSSI;
+            if (deviceName != null && deviceName.length() > 0)
+                viewHolder.deviceName.setText(deviceName);
+            else if(deviceRSSI != null && deviceRSSI.length() > 0)
+                viewHolder.deviceRSSI.setText(deviceRSSI);
+            else
+                viewHolder.deviceName.setText(R.string.unknown_device);
+            viewHolder.deviceAddress.setText(device.getAddress());
+
+            return view;
+        }
+    }
+
+    static class ViewHolder {
+        TextView deviceName;
+        TextView deviceAddress;
+        TextView deviceRSSI;
+    }
 }
